7e7d1aa87eb09f8230d1dd75c7f6ca439731b17b
Jitendra Kumar <164842901+Jgithub02@users.noreply.github.com>
2025-09-26 21:12:53 +0530

    Add files via upload

---
+++ Select&Where.sql
@@ -0,0 +1,71 @@
+SELECT * FROM employee_demographics;
+
+SELECT first_name 
+FROM employee_demographics;
+
+SELECT first_name, last_name, birth_date
+FROM employee_demographics;
+
+SELECT first_name, last_name, birth_date, age 
+FROM employee_demographics;
+
+SELECT first_name, last_name, birth_date, age, age + 10 as new_age 
+FROM employee_demographics;
+
+SELECT DISTINCT(gender) 
+FROM employee_demographics;
+
+-- WHERE CLAUSE
+
+SELECT * FROM employee_salary;
+
+SELECT * FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT * FROM employee_salary
+WHERE salary > 50000;
+
+SELECT * FROM employee_salary
+WHERE salary <= 50000;
+
+SELECT * FROM employee_salary
+WHERE dept_id = 1;
+
+SELECT * FROM employee_demographics
+WHERE gender = "Female";
+
+SELECT * FROM employee_demographics
+WHERE birth_date > "1985-01-01";
+
+-- AND, OR & NOT Operator (Logical operator)
+
+SELECT * FROM employee_demographics
+WHERE birth_date > "1985-01-01"
+AND gender = "Male";
+
+SELECT * FROM employee_demographics
+WHERE birth_date > "1985-01-01"
+OR gender = "Male";
+
+SELECT * FROM employee_demographics
+WHERE (first_name = "TOM" AND age = 36) OR age > 40;
+
+-- LIKE Statement
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE "Jer%";
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE "A%";
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE "%an%";
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE "A__";
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE "A__%";
+
+SELECT * FROM employee_demographics
+WHERE birth_date LIKE "1994%";
