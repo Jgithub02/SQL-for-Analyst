ee8333ee617769515797d2e4040b642269ea3ac3
Jitendra Kumar <164842901+Jgithub02@users.noreply.github.com>
2025-09-27 10:26:22 +0530

    Add files via upload

---
+++ Subqueries.sql
@@ -0,0 +1,52 @@
+SELECT * FROM 
+employee_demographics;
+
+SELECT * FROM 
+employee_demographics
+WHERE employee_id IN 
+				(SELECT employee_id 
+                 FROM employee_salary
+                 WHERE dept_id = 1 )
+;
+
+SELECT * FROM 
+employee_salary;
+
+SELECT first_name, salary,
+(SELECT AVG(salary) FROM 
+employee_salary) AS avg_salary
+FROM employee_salary;
+
+
+SELECT gender , AVG(age), MAX(age), MIN(age), COUNT(employee_id)
+FROM employee_demographics
+GROUP BY gender;
+
+
+SELECT * FROM
+(SELECT gender , AVG(age), MAX(age), MIN(age), COUNT(employee_id)
+FROM employee_demographics
+GROUP BY gender) AS agg_table;
+
+SELECT gender, AVG(`MAX(age)`) FROM
+(SELECT gender , AVG(age), MAX(age), MIN(age), COUNT(employee_id)
+FROM employee_demographics
+GROUP BY gender) AS agg_table
+GROUP BY gender;
+
+SELECT  AVG(max_age) FROM
+(SELECT gender , AVG(age), 
+MAX(age) AS max_age, 
+MIN(age) AS min_age, 
+COUNT(employee_id) AS total_emp
+FROM employee_demographics
+GROUP BY gender) AS agg_table
+;
+
+
+
+
+
+
+
+
